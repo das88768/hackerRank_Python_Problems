ef51331e3c38aa41d99d3ecda2ae31df2d31b9f0
Akash Das <das88768@gmail.com>
2023-07-13 19:59:52 +0530

    ready to commit

---
+++ loops.py
@@ -0,0 +1,7 @@
+# Loops
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(0, n):
+        print(i*i)
--- say_hello_world.py
+++ say_hello_world.py
@@ -1,2 +1,4 @@
+# Say "Hello, World!" With Python
+
 if __name__ == '__main__':
     print("Hello, World!")
